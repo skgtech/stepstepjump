d7f0c1d819480753da3839ef770e8b2cbb732c1b
Thanasis Polychronakis <thanpolas@gmail.com>
2014-12-06 20:29:27 +0200

    Create the base component

---
+++ jsapp/vector/components/base.comp.js
@@ -0,0 +1,62 @@
+/**
+
+ * @fileOverview The abstract component Ctor.
+ */
+
+var cip = require('cip');
+
+/**
+ * The abstract component Ctor.
+ *
+ * @constructor
+ */
+var Component = module.exports = cip.extend(function () {
+
+});
+
+/** @enum {string} The component types */
+Component.Type = {
+  LINE: 'line',
+  PLACEHOLDER_IF: 'placeholderIf',
+  PLACEHOLDER_IF_LOOP: 'placeholderIfLoop',
+  PLACEHOLDER_OPERATION: 'placeholderOperation',
+  OPERATION: 'operation',
+  IF_LOOP: 'ifLoop',
+};
+
+/**
+ * Get the type of the component.
+ *
+ * @return {app.vector.component.Base.Type} The type.
+ */
+Component.prototype.getType = function() {
+  throw new Error('Not Implemented');
+};
+
+/**
+ * Get the position of the component.
+ *
+ * @return {Array} The type.
+ */
+Component.prototype.getPosition = function() {
+  throw new Error('Not Implemented');
+};
+
+/**
+ * Calculate the outcome of passign through this component.
+ *
+ * @param {number} value The value to calculate.
+ * @return {number|boolean} The result depending on type.
+ */
+Component.prototype.calculate = function(/* arg */) {
+  throw new Error('Not Implemented');
+};
+
+/**
+ * Get the CPU Cost of this component.
+ *
+ * @return {number} The result.
+ */
+Component.prototype.getCpuCost = function() {
+  throw new Error('Not Implemented');
+};
