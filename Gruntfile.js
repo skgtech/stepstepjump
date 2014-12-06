25cf102825a18fad8c5a0d838aa604ce5cd8a833
Kostas Bariotis <konmpar@gmail.com>
2014-12-06 17:43:47 +0200

    fixed ejs compiled dest path

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function (grunt) {
     connect: {
       options: {
         port: 9000,
-        open: false,
+        open: true,
         livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
@@ -153,10 +153,11 @@ module.exports = function (grunt) {
     // Parse and compile .ejs Templates
     ejs: {
       all: {
-        src: ['templates/index.ejs', 'templates/board.ejs'],
-        dest: 'app/',
+        src: ['*.ejs'],
+        dest: '<%= config.app %>',
         expand: true,
-        ext: '.html'
+        ext: '.html',
+        cwd: 'templates'
       }
     },
 
@@ -236,7 +237,7 @@ module.exports = function (grunt) {
       options: {
         dest: '<%= config.dist %>'
       },
-      html: '<%= config.app %>/**/*.html'
+      html: '<%= config.app %>/*.html'
     },
 
     // Performs rewrites based on rev and the useminPrepare configuration
