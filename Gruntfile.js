b1752aab575bb8fd3b5118eb56b200adb64027c5
Thanasis Polychronakis <thanpolas@gmail.com>
2014-12-06 13:10:46 +0200

    new jshint rules

--- Gruntfile.js
+++ Gruntfile.js
@@ -286,6 +286,16 @@ module.exports = function (grunt) {
       }
     },
 
+    browserify: {
+      options: {
+      },
+      frontapp: {
+        files: {
+          'front/static/app.src.js': ['front/app/reveal.js'],
+        },
+      },
+    },
+
     // By default, your `index.html`'s <!-- Usemin block --> will take care
     // of minification. These next options are pre-configured if you do not
     // wish to use the Usemin blocks.
