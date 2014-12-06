f4f1e80ae402c41106ecd4947c08ff440d447f1c
Thanasis Polychronakis <thanpolas@gmail.com>
2014-12-06 21:12:51 +0200

    Scaffold the ball base

---
+++ jsapp/ball/ball.base.js
@@ -0,0 +1,44 @@
+/**
+ * @fileOverview The Ball base module.
+ */
+var cip = require('cip');
+
+var Component = require('../Level/components/base.comp');
+
+var Ball = module.exports = cip.extend(function () {
+  this.value = 0;
+});
+
+/**
+ * Master run method for ball.
+ *
+ * @param {app.Level} level Instance of Level.
+ */
+Ball.prototype.run = function(level) {
+  level.struct.forEach(function(component, index) {
+    if (Array.isArray(component)) {
+      return;
+    }
+
+    var res, next, route;
+
+    switch(component.getType()) {
+    case Component.Type.PLACEHOLDER_IF:
+      res = component.calculate(this.value);
+      next = index + 1;
+      route = res ? 1 : 0;
+      this.run(level.struct[next][route]);
+      break;
+    case Component.Type.PLACEHOLDER_IF_LOOP:
+      res = component.calculate(this.value);
+      next = index + 1;
+
+      // if true we need to loop.
+      route = res ? 1 : 0;
+
+      this.run(level.struct[next][route]);
+      break;
+
+    }
+  }, this);
+};
