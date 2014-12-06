b544ac7aea91a0e614c88eaf47161327ab8e833c
Aristotelis Dossas <dossas07@hotmail.com>
2014-12-06 13:57:41 +0200

    Add parse initialization

--- jsapp/boot.js
+++ jsapp/boot.js
@@ -3,3 +3,14 @@
  */
 
 console.log('Allo!');
+
+Parse.initialize('nu3rE4cjKRgg09PyOxqRrmncVeSeTHXrn7uqt8Cv', '5M7YXlxGkWenHq4MHVUJGhr3PJSOXgszCZymw0Jl');
+
+var Score = Parse.Object.extend('Score');
+var score = new Score();
+score.save({
+    'user': 'foo',
+    'score': 1200
+}).then(function(object) {
+  alert('yay! it worked');
+});
