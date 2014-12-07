9158124f670b3b5976610d10070febaec427af3f
Aristotelis Dossas <dossas07@hotmail.com>
2014-12-07 21:12:10 +0200

    Add example components

--- jsapp/toolbox/toolbox.base.js
+++ jsapp/toolbox/toolbox.base.js
@@ -13,7 +13,7 @@ var Toolbox = module.exports = function (vector) {
   /** @type {app.Vector} vector The vector instance */
   this.vector = vector;
   /** @type {number} width The width of the toolbox */
-  this.width = 300;
+  this.width = 100;
   /** @type {number} height The height of the toolbox */
   this.height = vector.getCanvasHeight();
   /** @type {number} x The left-most x coordinate */
@@ -26,11 +26,36 @@ var Toolbox = module.exports = function (vector) {
  * Draw the toolbox on the underlying vector library.
  */
 Toolbox.prototype.draw = function() {
+
   var level = new Level(this.vector);
 
   this.vector.makeRectangle(this.x, this.y, this.width, this.height);
 
-  level.makeIf(100, 60, 130, 80, 130, 40);
+  var upperMostY = this.y - this.height / 2,
+      drawY = upperMostY + 50,
+      drawX = this.x + 30;
+
+  level.makeIf(drawX, drawY);
+  drawY += 90;
+
+  level.makeIfLoop(drawX, drawY);
+  drawY += 90;
+
+  level.makeOperation(drawX, drawY);
+  drawY += 90;
+
+  level.makeIfLoop(drawX, drawY);
+  drawY += 90;
+
+  level.makeIf(drawX, drawY);
+  drawY += 90;
+
+  level.makeIfLoop(drawX, drawY);
+  drawY += 90;
+
+  level.makeOperation(drawX, drawY);
+  drawY += 90;
+
   var that = this;
   $(window).bind('click', function(e) {
     console.log(that.containsPoint(e.clientX, e.clientY));
