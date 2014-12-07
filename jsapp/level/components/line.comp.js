656f901ae8bdc7cae4dec78fc09d7b3b51bf323c
Aristotelis Dossas <dossas07@hotmail.com>
2014-12-07 06:11:30 +0200

    Use vector's makeline method

--- jsapp/level/components/line.comp.js
+++ jsapp/level/components/line.comp.js
@@ -33,7 +33,7 @@ Line.prototype.draw = function(x1, y1, x2, y2) {
   this.x2 = x2;
   this.y2 = y2;
 
-  this.vector.two.makeLine(x1, y1, x2, y2);
+  this.vector.makeLine(x1, y1, x2, y2);
 };
 
 /**
