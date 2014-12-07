e5054724bcb2086a14d76abf8a9841b6fdc3270c
Theodore Keloglou <theodorekeloglou@gmail.com>
2014-12-07 19:34:21 +0200

    add score model and score model instance

--- jsapp/boot.js
+++ jsapp/boot.js
@@ -12,3 +12,10 @@ level01.init();
 
 var $ = require('../bower_components/jquery/dist/jquery.js');
 $('#modalSaveToParse').modal('show');
+
+var scoreModel = require('./models/score.model.js');
+var scoreModelInstance = new scoreModel();
+
+// get name and score
+
+scoreModelInstance.save( /* name, score */);
