b1503426544fc0354affed4e32e2ba0094df1ff6
Kostas Bariotis <konmpar@gmail.com>
2014-12-07 20:09:58 +0200

    removed extra arguments from contructor

--- jsapp/level/components/if.comp.js
+++ jsapp/level/components/if.comp.js
@@ -33,15 +33,15 @@ var If = module.exports = Base.extend(function() {
  * @param {number} x3 The x3 position.
  * @param {number} y3 The y3 position.
  */
-If.prototype.draw = function(x1, y1, x2, y2, x3, y3) {
+If.prototype.draw = function(x1, y1) {
   this.x1 = x1;
   this.y1 = y1;
-  this.x2 = x2;
-  this.y2 = y2;
-  this.x3 = x3;
-  this.y3 = y3;
+  this.x2 = x1 + 30;
+  this.y2 = y1 - 30;
+  this.x3 = x1 + 30;
+  this.y3 = y1 + 30;
 
-  this.vector.makePolygon(x1, y1, x2, y2, x3, y3);
+  this.vector.makePolygon(this.x1,  this.y1, this.x2, this.y2, this.x3, this.y3);
 };
 
 /**
