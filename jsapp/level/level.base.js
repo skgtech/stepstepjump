2fd650de0b50cb0538838b2c0c7febd43c408646
Kostas Bariotis <konmpar@gmail.com>
2014-12-07 12:17:43 +0200

    added function to draw an If

--- jsapp/level/level.base.js
+++ jsapp/level/level.base.js
@@ -4,6 +4,7 @@
 var cip = require('cip');
 
 var Line = require('./components/line.comp');
+var If = require('./components/if.comp');
 
 /**
  * The base level designer.
@@ -31,6 +32,26 @@ var Level = module.exports = cip.extend(function (vector) {
 Level.prototype.makeLine = function(x1, x2, y1, y2) {
   var line = new Line(this.vector);
   line.draw(x1, x2, y1, y2);
+
+  this.struct.push(line);
+
+  return this;
+};
+
+/**
+ * Draw the component on the underlying vector library.
+ *
+ * @param {number} x1 The x1 position.
+ * @param {number} y1 The y1 position.
+ * @param {number} x2 The x2 position.
+ * @param {number} y2 The y2 position.
+ * @param {number} x3 The x3 position.
+ * @param {number} y3 The y3 position.
+ * @return {self} Chainable.
+ */
+Level.prototype.makeIf = function(x1, y1, x2, y2, x3, y3) {
+  var triangle = new If(this.vector);
+  triangle.draw(x1, y1, x2, y2, x3, y3);
 
   this.struct.push(line);
 
